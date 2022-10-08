fd141ac51bbf0ee7324b23e7239f621abacf2bc0
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-10-08 20:07:26 +0430

    postDetail ??? should be learnd and fixed

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -3,6 +3,28 @@ import React from 'react'
 
 
 const PostDetail = () => {
+  // const getContentFragment = (index, text, obj, type) => {
+  //   let modifiedText = text;
+  //   if(obj){
+  //     if(obj.bold){
+  //       modifiedText = (<b key={index}>{text}</b>);
+  //     }
+  //     if(obj.italic){
+  //       modifiedText = (<em key={index}>{text}</em>);
+  //     }
+  //     if(obj.underline){
+  //       modifiedText = (<u key={index}>{text}</u>);
+  //     }
+  //   }
+  //   switch (type) {
+  //     case 'heading-three':
+  //       return <h3 key={index} className="text-3xl font-semibold mb-4"
+    
+  //     default:
+  //       break;
+  //   }
+
+  // };
   return (
     <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md mb-6'>
@@ -34,7 +56,10 @@ const PostDetail = () => {
             </div>
           </div>
           <h1 className='mb-8 text-3xl font-semibold'>{post.title}</h1>
-          {post.content.raw.children.map((typeObj, index))}
+          {/* {post.content.raw.children.map((typeObj, index)=> {
+            const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
+            return getContentFragment(index, children, typeObj, typeObject.type)
+          })} */}
         </div>
       </div>
     </div>
